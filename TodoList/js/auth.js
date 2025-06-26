41fb086342d2dcd23cb4ea6f05cdc91f79b06715
kchagas94 <kenedychagas@gmail.com>
2025-06-26 20:25:58 -0300

    add-verificação-email

--- TodoList/js/auth.js
+++ TodoList/js/auth.js
@@ -1,3 +1,7 @@
+
+
+
+// Função que trata a submissão do formulário de autenticação
 authForm.onsubmit = function (event) {
   showItem(loading)
   event.preventDefault()
@@ -5,22 +9,45 @@ authForm.onsubmit = function (event) {
     firebase.auth().signInWithEmailAndPassword(authForm.email.value, authForm.password.value).catch(function (error) {
       console.log('Falha no acesso')
       console.log(error)
+      hideItem(loading)
     })
   } else {
     firebase.auth().createUserWithEmailAndPassword(authForm.email.value, authForm.password.value).catch(function (error) {
       console.log('Falha no cadastro')
       console.log(error)
+      hideItem(loading)
     })
   }
 }
 
+// Função que centraliza e trata a autenticação
 firebase.auth().onAuthStateChanged(function (user) {
   hideItem(loading)
   if (user) {
-    console.log('Usuário autenticado')
-    console.log(user)
+    showUserContent(user)
   } else {
-    console.log('Usuário não autenticado')
+    showAuth()
   }
 })
 
+// Função que permite ao usuário sair da conta dele
+function signOut() {
+  firebase.auth().signOut().catch(function (error) {
+    console.log('Falha ao sair da conta')
+    console.log(error)
+  })
+}
+
+// Função que permite o usuário fazer a verificação do e-mail dele
+function sendEmailVerification() {
+  showItem(loading)
+  var user = firebase.auth().currentUser
+  user.sendEmailVerification().then(function () {
+    alert('E-mail de verificação foi enviado para ' + user.email + '! Verifique a sua caixa de entrada')
+  }).catch(function (error) {
+    alert('Houve um erro ao enviar o e-mail de verificação')
+    console.log(error)
+  }).finally(function () {
+    hideItem(loading)
+  })
+}
--- TodoList/js/utils.js
+++ TodoList/js/utils.js
@@ -6,6 +6,14 @@ var access = document.getElementById('access')
 
 var loading = document.getElementById('loading')
 
+var auth = document.getElementById('auth')
+var userContent = document.getElementById('userContent')
+
+var userEmail = document.getElementById('userEmail')
+
+var sendEmailVerificationDiv = document.getElementById('sendEmailVerificationDiv')
+var emailVerified = document.getElementById('emailVerified')
+
 // Alterar o formulário de autenticação para o cadastro de novas contas
 function toggleToRegister() {
   authForm.submitAuthForm.innerHTML = 'Cadastrar conta'
@@ -30,4 +38,27 @@ function showItem(element) {
 // Simplifica a remoção de elementos da página
 function hideItem(element) {
   element.style.display = 'none'
+}
+
+// Mostrar conteúdo para usuários autenticados
+function showUserContent(user) {
+  console.log(user)
+  if (user.emailVerified) {
+    emailVerified.innerHTML = 'E-mail verificado'
+    hideItem(sendEmailVerificationDiv)
+  } else {
+    emailVerified.innerHTML = 'E-mail não verificado'
+    showItem(sendEmailVerificationDiv)
+  }
+  userEmail.innerHTML = user.email
+  hideItem(auth)
+  showItem(userContent)
+}
+
+// Mostrar conteúdo para usuários não autenticados
+function showAuth() {
+  authForm.email.value = ''
+  authForm.password.value = ''
+  hideItem(userContent)
+  showItem(auth)
 }
